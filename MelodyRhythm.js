29e54df69cb507273952cecbf3ddf6fc67383456
Peter <112364552+p-Lennart@users.noreply.github.com>
2024-09-24 01:05:14 -0400

    MelodyRhythm method syncopatedAccomp()
    Returns AccompRhythm syncopating the melody

--- MelodyRhythm.js
+++ MelodyRhythm.js
@@ -1,3 +1,4 @@
+const AccompRhythm = require("./AccompRhythm");
 const RhythmPattern = require("./RhythmPattern");
 
 class MelodyRhythm extends RhythmPattern {
@@ -19,6 +20,19 @@ class MelodyRhythm extends RhythmPattern {
     static presets = [
         "s-ww,s-s-,s-ws,-s-w",
     ];
+
+    syncopatedAccomp() {
+        let resVal = this.toString();
+        
+        resVal = resVal.replaceAll("-", "u"); // Fill rests with upbeats
+        resVal = resVal.replaceAll("w", "u"); // Fill weak beats with upbeats
+        resVal = resVal.replaceAll("s", "-"); // Rest on strong beats
+        
+        resVal = "d" + resVal.slice(1); // Start with downbeat
+
+        let res = new AccompRhythm(resVal);
+        return res;
+    }
 
 }
 
